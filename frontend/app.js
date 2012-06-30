ed1ba1e004ce1ec7929743545612ff1fda603e69
Benajnim <benajnim@gmail.com>
2012-06-29 19:19:22 -0500

    mongolab connectivity

--- frontend/app.js
+++ frontend/app.js
@@ -4,57 +4,61 @@ var express = require('express')
   , mongo = require('mongodb')
   , ObjectID = require('mongodb').ObjectID;
 
-
-db = new mongo.Db('dailyblocks', new mongo.Server("127.0.0.1", 27017, {}), {});
+db = new mongo.Db('dailyblocks', new mongo.Server("ds033767.mongolab.com", 33767, {auto_reconnect: true}), {});
 
 db.addListener("error", function(error) {
   console.log("Error connecting to mongo - perhaps it isn't running?");
 });
 
 db.open(function(p_db) {
-	var app = express.createServer();
+  db.authenticate("dailyblocks", "d41lybl0ck5", function(p_db) {
 
-    // configure Express
-    app.configure(function() {
-      // app.set('views', __dirname + '/views');
-      // app.set('view engine', 'ejs');
-      app.use(express.logger());
-      app.use(express.cookieParser());
-      app.use(express.bodyParser());
-      app.use(express.methodOverride());
-      // app.use(express.session({ secret: 'keyboard cat' }));
+  	var app = express.createServer();
 
-      app.use(app.router);
-      // app.use(express.static(__dirname + '/static'));
-    });
+      // configure Express
+      app.configure(function() {
+        // app.set('views', __dirname + '/views');
+        // app.set('view engine', 'ejs');
+        app.use(express.logger());
+        app.use(express.cookieParser());
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        // app.use(express.session({ secret: 'keyboard cat' }));
 
-	    // function (error, results) {
-	    //         res.json(results);
-	    //       }
-
-    app.get('/all', function(req, res){
-      db.collection('feeds', function(error, feed_collection) {
-        feed_collection.find().toArray(function (error, results) {
-            res.json(results);
-          });
+        app.use(app.router);
+        app.use(express.static(__dirname + '/assets'));
       });
-    });
 
-    app.get('/all/:id', function (req, res) {
-    	db.collection('feeds', function(error, feed_collection) {
-        feed_collection.find({user_id:ObjectID(req.params.user_id)}).toArray(function (error, results) {
-            res.json(results);
-          });
+  	  app.get('/', function (req, res) {
+        res.json("hi");
       });
-    });
 
-    app.get('/item/:id', function (req, res) {
-    	db.collection('feeds', function(error, feed_collection) {
-        feed_collection.find({_id:ObjectID(req.params.id)}).toArray(function (error, results) {
-            res.json(results);
-          });
+      app.get('/feeds/all', function(req, res){
+        db.collection('feeds', function(error, feed_collection) {
+          feed_collection.find().toArray(function (error, results) {
+              res.json(results);
+            });
+        });
       });
-    });
 
-    app.listen(3000);
+      
+
+      app.get('/all/:id', function (req, res) {
+      	db.collection('feeds', function(error, feed_collection) {
+          feed_collection.find({user_id:ObjectID(req.params.user_id)}).toArray(function (error, results) {
+              res.json(results);
+            });
+        });
+      });
+
+      app.get('/item/:id', function (req, res) {
+      	db.collection('feeds', function(error, feed_collection) {
+          feed_collection.find({_id:ObjectID(req.params.id)}).toArray(function (error, results) {
+              res.json(results);
+            });
+        });
+      });
+
+      app.listen(3000);
+  });
 });
