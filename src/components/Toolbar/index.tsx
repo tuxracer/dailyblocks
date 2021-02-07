0b13904974eedab498ad8f632f0b9cac43edf410
Derek Petersen <derek@typescript.engineer>
2021-02-07 01:51:24 -0800

    add toggle fullscreen button

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,4 +1,5 @@
 import { FunctionalComponent, h, Fragment } from "preact";
+import { useState } from "preact/compat";
 import { usePost } from "../../hooks/reddit";
 import { playNext } from "../Thumbnails";
 
@@ -11,6 +12,18 @@ export const Toolbar: FunctionalComponent<ToolbarProps> = ({
     subreddit,
     postId
 }) => {
+    const [isFullscreen, setIsFullscreen] = useState<boolean>(false);
+
+    const toggleFullscreen = () => {
+        if (isFullscreen) {
+            document.exitFullscreen();
+            setIsFullscreen(false);
+        } else {
+            document.documentElement.requestFullscreen();
+            setIsFullscreen(true);
+        }
+    };
+
     const { data: redditPost, error, isLoading } = usePost({
         id: postId,
         subreddit
@@ -24,6 +37,11 @@ export const Toolbar: FunctionalComponent<ToolbarProps> = ({
                 {isLoadedSuccessfully && subreddit}
             </div>
             <div>{redditPost?.title}</div>
+            <div class="icons">
+                <button onClick={toggleFullscreen}>
+                    <img src="/assets/fullscreen.svg" />
+                </button>
+            </div>
         </Fragment>
     );
 };
