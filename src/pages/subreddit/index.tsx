66b292da7bc2b4882c8ac9ec2d87e09080a62d4f
Derek Petersen <jupiterspaceport@protonmail.com>
2022-05-24 08:54:08 -0700

    return NotFound for blacklisted pages

--- src/pages/subreddit/index.tsx
+++ src/pages/subreddit/index.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FunctionalComponent, h } from "preact";
 import Match from "preact-router/match";
 import { Player } from "../../components/Player";
 import { Thumbnails } from "../../components/Thumbnails";
 import { Toolbar } from "../../components/Toolbar";
 import { Comments } from "../../components/Comments";
+import { BLACKLIST } from "../../common/consts";
+import NotFound from "../NotFound";
 
 interface SubredditProps {
     subreddit?: string;
@@ -13,26 +16,32 @@ interface SubredditProps {
 const Subreddit: FunctionalComponent<SubredditProps> = ({
     subreddit = "videos",
     postId
-}) => (
-    <div class="subreddit">
-        <header>
-            <Toolbar postId={postId} subreddit={subreddit} />
-        </header>
-        <section>
-            <nav>
-                <Thumbnails subreddit={subreddit} />
-            </nav>
-            <main>
-                <Player postId={postId} subreddit={subreddit} />
-            </main>
-            <aside>
-                <Match path="/">
-                    {({ path }: any) => <Comments permalink={path} />}
-                </Match>
-            </aside>
-        </section>
-    </div>
-);
+}) => {
+    if (BLACKLIST.includes(postId || "")) {
+        return <NotFound />;
+    }
+
+    return (
+        <div class="subreddit">
+            <header>
+                <Toolbar postId={postId} subreddit={subreddit} />
+            </header>
+            <section>
+                <nav>
+                    <Thumbnails subreddit={subreddit} />
+                </nav>
+                <main>
+                    <Player postId={postId} subreddit={subreddit} />
+                </main>
+                <aside>
+                    <Match path="/">
+                        {({ path }: any) => <Comments permalink={path} />}
+                    </Match>
+                </aside>
+            </section>
+        </div>
+    );
+};
 
 export { Subreddit };
 
