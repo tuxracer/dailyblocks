706f70b670225c5f7200c332e223ff4b2c4fdea0
Derek Petersen <jupiterspaceport@protonmail.com>
2022-05-24 08:54:44 -0700

    remove unnecessary import from NotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,5 @@
 import { FunctionalComponent, h } from "preact";
 import { Link } from "preact-router/match";
-import * as style from "./style.css";
 
 export const NotFound: FunctionalComponent = () => {
     return (
