44771ff947ec841c3706d2b35d0dee5a9dc13c70
Derek Petersen <tuxracer@gmail.com>
2025-11-27 20:20:04 -0800

    redirect from /r/subreddit -> first post permalink

--- src/routes/r.$subreddit.index.tsx
+++ src/routes/r.$subreddit.index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Navigate } from '@tanstack/react-router'
 import { useSubreddit } from '../hooks/useSubreddit'
 
 export const Route = createFileRoute('/r/$subreddit/')({
@@ -8,17 +8,15 @@ export const Route = createFileRoute('/r/$subreddit/')({
 function SubredditPage() {
   const params = Route.useParams()
   const subreddit = useSubreddit({ subreddit: params.subreddit })
+  const firstPostPermalink = subreddit.data?.[0]?.permalink
 
-  return (
-    <div className="p-4">
-      <h2 className="text-xl font-bold">r/{params.subreddit}</h2>
-      {subreddit.isLoading && <p>Loading...</p>}
-      {subreddit.error && <p>Error: {subreddit.error.message}</p>}
-      <ul>
-        {subreddit.data?.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
-  )
+  if (subreddit.isLoading) {
+    return null
+  }
+
+  if (!firstPostPermalink) {
+    return "No videos found for this subreddit"
+  }
+
+  return <Navigate to={firstPostPermalink} replace />
 }
